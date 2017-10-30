0a7278db283000c54074a3c116c303f16e4dcb15
Bryan Brotonel <mrbryanbrotonel@gmail.com>
2017-10-29 22:27:29 -0700

    initial commit

---
+++ index.php
@@ -0,0 +1,67 @@
+<html>
+   <head>
+      <title>Colledge</title>
+      <meta name = 'viewport' content = 'width = device-width, initial-scale = 1'>
+      <link rel = 'stylesheet' href = 'https://fonts.googleapis.com/icon?family=Material+Icons'>
+      <link rel = 'stylesheet' href = 'https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.3/css/materialize.min.css'>
+      <link rel = 'stylesheet' href = 'css/style.css'>
+      <script type = 'text/javascript'src = 'https://code.jquery.com/jquery-2.1.1.min.js'></script>
+      <script src = 'https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.3/js/materialize.min.js'></script>
+      <script type = 'text/javascript' src = 'js/main.js'></script>
+   </head>
+
+   <script>
+   $(document).ready(function() {
+    $('select').material_select();
+  });
+  </script>
+
+  <nav class="pink accent-1" role="navigation">
+    <div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo">Colledge</a>
+         <ul class="right hide-on-med-and-down">
+           <!-- <li><a href="#">Back</a></li> -->
+         </ul>
+
+         <ul id="nav-mobile" class="side-nav">
+           <!-- <li><a href="#">Back</a></li> -->
+         </ul>
+         <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
+       </div>
+     </nav>
+
+     <div class="container">
+       <br>
+
+
+     <div class="container">
+     </div>
+   </div>
+
+     <footer class="page-footer orange">
+       <div class="container">
+         <div class="row">
+           <div class="col l6 s12">
+             <h5 class="white-text">Who Are We?</h5>
+             <p class="grey-text text-lighten-4">We are one hour start up by a pair of college students.</p>
+
+
+           </div>
+           <div class="col l3 s12">
+             <h5 class="white-text">Connect</h5>
+             <ul>
+               <li><a class="white-text" href="#!">Facebook</a></li>
+               <li><a class="white-text" href="#!">Twitter</a></li>
+               <li><a class="white-text" href="#!">Snapchat</a></li>
+               <li><a class="white-text" href="#!">Instagram</a></li>
+             </ul>
+           </div>
+         </div>
+       </div>
+       <div class="footer-copyright">
+         <div class="container">
+         Made by <a class="orange-text text-lighten-3" href="http://materializecss.com">Materialize</a>
+         </div>
+       </div>
+     </footer>
+
+</html>
