9794d4c272ab5f9ac0a2643b1550baa8b638bce8
Bryan Brotonel <mrbryanbrotonel@gmail.com>
2017-10-29 23:35:59 -0700

    updated website front-end

--- index.php
+++ index.php
@@ -5,63 +5,123 @@
       <link rel = 'stylesheet' href = 'https://fonts.googleapis.com/icon?family=Material+Icons'>
       <link rel = 'stylesheet' href = 'https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.3/css/materialize.min.css'>
       <link rel = 'stylesheet' href = 'css/style.css'>
-      <script type = 'text/javascript'src = 'https://code.jquery.com/jquery-2.1.1.min.js'></script>
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
       <script src = 'https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.3/js/materialize.min.js'></script>
+      <script src="js/typed.min.js" type="text/javascript"></script>
       <script type = 'text/javascript' src = 'js/main.js'></script>
    </head>
 
    <script>
    $(document).ready(function() {
     $('select').material_select();
+
+    var typed = new Typed('#typed', {
+      stringsElement: '#typed-strings',
+      startDelay: 2000,
+      typeSpeed: 140,
+      backSpeed: 140,
+      showCursor: false,
+      smartBackspace: true,
+      backDelay: 800,
+    });
   });
   </script>
 
-  <nav class="pink accent-1" role="navigation">
-    <div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo">Colledge</a>
-         <ul class="right hide-on-med-and-down">
-           <!-- <li><a href="#">Back</a></li> -->
-         </ul>
+  <nav class="pink accent-1" role="navigation"></nav>
 
-         <ul id="nav-mobile" class="side-nav">
-           <!-- <li><a href="#">Back</a></li> -->
-         </ul>
-         <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
+     <body class="grey lighten-5">
+       <div class="hero-header">
+
        </div>
-     </nav>
-
-     <div class="container">
-       <br>
-
-
-     <div class="container">
-     </div>
-   </div>
-
-     <footer class="page-footer orange">
+     <div class="section no-pad-bot" id="index-banner">
        <div class="container">
+         <br><br>
+           <div id="typed-strings">
+             <p>College</p>
+             <p>Colle^1900dge</p>
+           </div>
+         <h1 id="typed" class="header center orange-text">College</h1>
+         <div class="row center">
+           <h5 class="header col s12 light">We give you the edge in your colledge search</h5>
+         </div>
+         <br>
+       </div>
+     </div>
+
+
+     <div class="container">
+       <div class="section">
+
+         <!--   Icon Section   -->
          <div class="row">
-           <div class="col l6 s12">
-             <h5 class="white-text">Who Are We?</h5>
-             <p class="grey-text text-lighten-4">We are one hour start up by a pair of college students.</p>
+           <div class="col s12 m4">
+             <div class="icon-block">
+               <h2 class="center pink-text text-accent-1"><i class="medium material-icons">flash_on</i></h2>
+               <h5 class="center">Smart</h5>
 
-
+               <p class="light">We did most of the heavy lifting for you to provide a default stylings that incorporate our custom components. Additionally, we refined animations and transitions to provide a smoother experience for developers.</p>
+             </div>
            </div>
-           <div class="col l3 s12">
-             <h5 class="white-text">Connect</h5>
-             <ul>
-               <li><a class="white-text" href="#!">Facebook</a></li>
-               <li><a class="white-text" href="#!">Twitter</a></li>
-               <li><a class="white-text" href="#!">Snapchat</a></li>
-               <li><a class="white-text" href="#!">Instagram</a></li>
-             </ul>
+
+           <div class="col s12 m4">
+             <div class="icon-block">
+               <h2 class="center pink-text text-accent-1"><i class="medium material-icons">portrait</i></h2>
+               <h5 class="center">Just For You</h5>
+
+               <p class="light">By utilizing elements and principles of Material Design, we were able to create a framework that incorporates components and animations that provide more feedback to users. Additionally, a single underlying responsive system across all platforms allow for a more unified user experience.</p>
+             </div>
+           </div>
+
+           <div class="col s12 m4">
+             <div class="icon-block">
+               <h2 class="center pink-text text-accent-1"><i class="medium material-icons">sentiment_very_satisfied</i></h2>
+               <h5 class="center">Satisfaction Guaranteed</h5>
+
+               <p class="light">We have provided detailed documentation as well as specific code examples to help new users get started. We are also always open to feedback and can answer any questions a user may have about Materialize.</p>
+             </div>
            </div>
          </div>
+
        </div>
-       <div class="footer-copyright">
-         <div class="container">
-         Made by <a class="orange-text text-lighten-3" href="http://materializecss.com">Materialize</a>
+       <br><br>
+     </div>
+     <div class="row center">
+       <a href="form.html" id="download-button" class="btn-large waves-effect waves-light orange">Get Started!</a>
+     </div>
+     <br><br>
+   </body>
+
+   <footer class="page-footer orange">
+     <div class="container">
+       <div class="row">
+         <div class="col s6 left-align">
+           <h5 class="white-text">Who Are We?</h5>
+           <p class="grey-text text-lighten-4">We are one hour start up by a pair of college students.</p>
+         </div>
+         <div class="col s6 right-align">
+           <h5 class="white-text">Connect</h5>
+           <ul>
+             <li><a class="white-text" href="https://www.facebook.com/">Facebook</a></li>
+             <li><a class="white-text" href="https://twitter.com/">Twitter</a></li>
+             <li><a class="white-text" href="https://www.instagram.com/">Instagram</a></li>
+           </ul>
          </div>
        </div>
-     </footer>
+     </div>
+     <div class="footer-copyright">
+       <div class="container">
+         Styling by the wonderfuls at  <a class="orange-text text-lighten-3" href="http://materializecss.com">Materialize</a>
+       </div>
+     </div>
+   </footer>
 
 </html>
+
+<script>
+$('#typed').typeIt({
+  startDelay: 10000,
+  startDelete: true,
+  speed: 100,
+  strings: 'Colledge'
+});
+</script>
--- submit.php
+++ submit.php
@@ -27,10 +27,10 @@
      <div class="section no-pad-bot" id="index-banner">
        <div class="container">
          <br><br>
-         <h1 class="header center orange-text">******</h1>
          <div class="row center">
-           <h5 class="header col s12 light">Why is this the best college for you?</h5>
+           <h4 class="header col s12 light">Your lucky colledge is:</h4>
          </div>
+         <h1 class="header center orange-text">******</h1>
          <div class="row center">
            <a href="https://bcit.ca" id="download-button" class="btn-large waves-effect waves-light orange">Find out!</a>
          </div>
@@ -47,27 +47,24 @@
          <div class="row">
            <div class="col s12 m4">
              <div class="icon-block">
-               <h2 class="center light-blue-text"><i class="material-icons">flash_on</i></h2>
-               <h5 class="center">Speeds up development</h5>
-
+               <h2 class="center pink-text text-accent-1"><i class="medium material-icons">school</i></h2>
+               <h5 class="center">Amazing Faculty</h5>
                <p class="light">We did most of the heavy lifting for you to provide a default stylings that incorporate our custom components. Additionally, we refined animations and transitions to provide a smoother experience for developers.</p>
              </div>
            </div>
 
            <div class="col s12 m4">
              <div class="icon-block">
-               <h2 class="center light-blue-text"><i class="material-icons">group</i></h2>
-               <h5 class="center">User Experience Focused</h5>
-
+               <h2 class="center pink-text text-accent-1"><i class="medium material-icons">location_city</i></h2>
+               <h5 class="center">Oh, The Places You'll Go!</h5>
                <p class="light">By utilizing elements and principles of Material Design, we were able to create a framework that incorporates components and animations that provide more feedback to users. Additionally, a single underlying responsive system across all platforms allow for a more unified user experience.</p>
              </div>
            </div>
 
            <div class="col s12 m4">
              <div class="icon-block">
-               <h2 class="center light-blue-text"><i class="material-icons">settings</i></h2>
-               <h5 class="center">Easy to work with</h5>
-
+               <h2 class="center pink-text text-accent-1"><i class="medium material-icons">people</i></h2>
+               <h5 class="center">You'll Fit Right In</h5>
                <p class="light">We have provided detailed documentation as well as specific code examples to help new users get started. We are also always open to feedback and can answer any questions a user may have about Materialize.</p>
              </div>
            </div>
@@ -80,26 +77,23 @@
      <footer class="page-footer orange">
        <div class="container">
          <div class="row">
-           <div class="col l6 s12">
+           <div class="col s6 left-align">
              <h5 class="white-text">Who Are We?</h5>
              <p class="grey-text text-lighten-4">We are one hour start up by a pair of college students.</p>
-
-
            </div>
-           <div class="col l3 s12">
+           <div class="col s6 right-align">
              <h5 class="white-text">Connect</h5>
              <ul>
-               <li><a class="white-text" href="#!">Facebook</a></li>
-               <li><a class="white-text" href="#!">Twitter</a></li>
-               <li><a class="white-text" href="#!">Snapchat</a></li>
-               <li><a class="white-text" href="#!">Instagram</a></li>
+               <li><a class="white-text" href="https://www.facebook.com/">Facebook</a></li>
+               <li><a class="white-text" href="https://twitter.com/">Twitter</a></li>
+               <li><a class="white-text" href="https://www.instagram.com/">Instagram</a></li>
              </ul>
            </div>
          </div>
        </div>
        <div class="footer-copyright">
          <div class="container">
-         Made by <a class="orange-text text-lighten-3" href="http://materializecss.com">Materialize</a>
+           Styling by the wonderfuls at  <a class="orange-text text-lighten-3" href="http://materializecss.com">Materialize</a>
          </div>
        </div>
      </footer>
