88317530ae1c2700c662fdd104ffaf4ea790b37d
Bryan Brotonel <mrbryanbrotonel@gmail.com>
2017-11-02 11:36:08 -0700

    added favicon, google analytics

--- js/main.js
+++ js/main.js
@@ -1,15 +1,26 @@
 $(document).ready(function() {
 
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyAzzTPmuK7-KKQ3DoN0m3uBuhGokaUBzZA",
-    authDomain: "colledge-f0bab.firebaseapp.com",
-    databaseURL: "https://colledge-f0bab.firebaseio.com",
-    projectId: "colledge-f0bab",
-    storageBucket: "colledge-f0bab.appspot.com",
-    messagingSenderId: "26297603867"
-  };
-  firebase.initializeApp(config);
+	// Initialize Firebase
+	var config = {
+		apiKey: "AIzaSyAzzTPmuK7-KKQ3DoN0m3uBuhGokaUBzZA",
+		authDomain: "colledge-f0bab.firebaseapp.com",
+		databaseURL: "https://colledge-f0bab.firebaseio.com",
+		projectId: "colledge-f0bab",
+		storageBucket: "colledge-f0bab.appspot.com",
+		messagingSenderId: "26297603867"
+	};
+	firebase.initializeApp(config);
 
- $('select').material_select();
+	//<Material Select
+	$('select').material_select();
+
+	// Global site tag (gtag.js) - Google Analytics
+	window.dataLayer = window.dataLayer || [];
+
+	function gtag() {
+		dataLayer.push(arguments);
+	}
+	gtag('js', new Date());
+
+	gtag('config', 'UA-108344152-2');
 });
